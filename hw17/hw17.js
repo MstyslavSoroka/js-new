955b1525b3ada64837bc721e91daa826fdbe969c
mstyslav <mstyslav2010@gmail.com>
2024-08-10 10:49:08 +0100

    hw17

---
+++ hw17/hw17.js
@@ -0,0 +1,31 @@
+
+const myButton = document.getElementById('myButton');
+const myP = document.getElementById('myP');
+const pContent = myP.textContent
+myButton.textContent = pContent
+
+
+
+
+// ======================================1====================================
+
+const img = document.getElementById("img")
+img.src ="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"
+
+// ======================================2====================================
+
+const image =document.getElementById("beluga")
+image.alt = "beluga"
+
+const link = document.getElementById("link")
+link.href = "https://www.opera.com/ua/gx"
+
+
+// ======================================3====================================
+
+let firstElem = document.querySelector(`li`);
+
+firstElem.textContent = "New Text"
+
+// ======================================4====================================
+
