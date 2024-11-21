ac47cdb99501cd10ee06c1bd7ada85f8250bbe2d
Mstyslav Soroka <mstyslav2010@gmail.com>
2024-11-21 12:27:11 +0000

    hw26

---
+++ hw26/src/data.js
@@ -0,0 +1,11 @@
+export const data = {
+  name: '',
+  email: '',
+  pass: '',
+};
+
+export function setData(name, email, pass) {
+  data.name = name;
+  data.email = email;
+  data.pass = pass;
+}
